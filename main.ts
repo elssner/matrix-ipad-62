3e1f684e26a2f7b7564e2b94dc6c8cc6339a2a9b
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-03-19 13:31:41 -0700

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    matrix.clearMatrix()
+    matrix.rasterCircle(32, 32, 32)
+    matrix.rasterCircle(32, 32, 30)
+    matrix.line(10, 60, 100, 9, true)
+    matrix.writeDisplay()
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     matrix.clearMatrix()
     i1 = matrix.matrix16x8(`
