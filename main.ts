d17f1beaca53ead9b3ba765f53a816a161e2d8a9
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-05-22 22:04:42 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,49 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    matrix.clearMatrix()
-    matrix.rasterCircle(32, 32, 32)
-    matrix.rasterCircle(32, 32, 30)
-    matrix.line(10, 60, 100, 9, true)
-    matrix.writeDisplay()
+input.onGesture(Gesture.TiltRight, function () {
+    richtung = 1
+    basic.showIcon(IconNames.ArrowSouthEast)
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    matrix.clearMatrix()
-    i1 = matrix.matrix16x8(`
-        # # # . . . . . . . . . . # # #
-        # . . . . . . . . . . . . . . #
-        # . . . . . . . . . . . . . . #
-        . . . . . . . # # . . . . . . .
-        . . . . . . . # # . . . . . . .
-        # . . . . . . . . . . . . . . #
-        # . . . . . . . . . . . . . . #
-        # # # . . . . . . . . . . # # #
-        `)
-    matrix.writeImage(i1, 0, 0, 7)
-    matrix.writeDisplay()
+function moveY () {
+    y += richtung
+    matrix.setPixel(x, y, true)
+    matrix.displayMatrixChangedPages()
+}
+function moveX () {
+    x += richtung
+    matrix.setPixel(x, y, true)
+    matrix.displayMatrixChangedPages()
+}
+input.onGesture(Gesture.TiltLeft, function () {
+    richtung = -1
+    basic.showIcon(IconNames.ArrowNorthWest)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    richtung = richtung * -1
 })
 function Konfiguration () {
     matrix.comment("elssner/matrix-ipad-62")
     matrix.comment("1 Erweiterung:")
     matrix.comment("calliope-net/matrix")
 }
-let i1: Image = null
-matrix.init(matrix.ePages.y64)
-matrix.clearMatrix()
+input.onButtonEvent(Button.AB, input.buttonEventValue(ButtonEvent.Hold), function () {
+    matrix.clearMatrix()
+    matrix.displayMatrix()
+})
+let y = 0
+let x = 0
+let richtung = 0
+matrix.init(matrix.ePages.y128)
+matrix.displayMatrix()
+richtung = 1
+x = 0
+y = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
+        basic.setLedColors(0xff0000, 0x000000, 0x000000)
+        moveX()
+    } else if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
+        basic.setLedColors(0x000000, 0x000000, 0xff0000)
+        moveY()
+    } else {
+        basic.setLedColors(0x000000, 0x000000, 0x000000)
+    }
+})
